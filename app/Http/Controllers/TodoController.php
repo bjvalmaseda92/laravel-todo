1717b1ec30860edaebf99a4250246cadc3d3cb5b
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-01 14:57:13 -0500

    refactor(validate): Add validation in Request File

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoCreateRequest;
 use App\Models\Todo;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -16,21 +15,10 @@ class TodoController extends Controller
         return view('todos.create');
     }
 
-    public function store(Request $request){
+    public function store(TodoCreateRequest $request){
 
-        // $request->validate([
-        //     'title' => 'required|max:255'
-        // ]);
-        // Todo::create($request->all());
+        Todo::create($request->all());
 
-        $rules=[
-                 'title' => 'required|max:255'
-        ];
-
-        $messages=[
-            'title.max' => 'Todo title should not be greater than 255 chars'
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
         return redirect()->back()->with('message', 'Todo Created Successfully');
     }
 
