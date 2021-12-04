0082366f7c0e332690bd29a6935426f35332c80e
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-04 12:23:40 -0500

    feat(todos): 💄 Show the steps for every Todo

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -11,6 +11,20 @@
 
 <x-alert />
 <div>
-    <p>{{$todo->description}}</p>
+    <div>
+        <h3 class="text-lg">Description</h3>
+
+        <p>{{$todo->description}}</p>
+    </div>
+
+    @if ($todo->steps->count()>0)
+    <div class="py-4">
+        <h3 class="text-lg">Steps for this task</h3>
+        @foreach ($todo->steps as $step)
+        <p>{{$step->name}}</p>
+        @endforeach
+    </div>
+    @endif
+</div>
 </div>
 @endsection
