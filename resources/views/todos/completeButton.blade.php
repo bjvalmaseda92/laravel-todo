fd563b3f322aee5aad373624708a5b657d8b63d4
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-04 12:42:22 -0500

    feat(steps): ✨ Add steps to the db
    - Add steps to the db from task
    
    - Fix the ui problem when you delete steps

--- resources/views/todos/completeButton.blade.php
+++ resources/views/todos/completeButton.blade.php
@@ -1,19 +1,18 @@
 @if ($todo->completed)
 <span class="fas fa-check px-2 text-green-400 cursor-pointer" onclick="event.preventDefault(); 
-    document.getElementById('form-incomplete-{{ $todo->id }}').submit()""/> 
-    <form method=" post" id="{{ 'form-incomplete-' . $todo->id }}" class="hidden"
+    document.getElementById('form-incomplete-{{ $todo->id }}').submit()" />
+<form method="post" id="{{ 'form-incomplete-' . $todo->id }}" class="hidden"
     action="{{ route('todo.incomplete', $todo) }}">
     @csrf
     @method('delete')
-    </form>
+</form>
 
-    @else
-    <span onclick="event.preventDefault(); 
+@else
+<span onclick="event.preventDefault(); 
                 document.getElementById('form-complete-{{ $todo->id }}').submit()"
-        class="fas fa-check px-2 text-gray-300 cursor-pointer" />
-    <form method="post" id="{{ 'form-complete-' . $todo->id }}" class="hidden"
-        action="{{ route('todo.complete', $todo) }}">
-        @csrf
-        @method('put')
-    </form>
-    @endif
+    class="fas fa-check px-2 text-gray-300 cursor-pointer" />
+<form method="post" id="{{ 'form-complete-' . $todo->id }}" class="hidden" action="{{ route('todo.complete', $todo) }}">
+    @csrf
+    @method('put')
+</form>
+@endif
