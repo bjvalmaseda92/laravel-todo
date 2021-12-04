06d3ee3380014f6f554f38fc4f74e7b3dbcb6221
Barbaro Javier <bjvalmaseda.g@gmail.com>
2021-12-04 11:01:56 -0500

    build(todos): ➕ Add livewire package

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -16,12 +16,17 @@
         <input type="text" name="title" id="title" class="py-2 border rounded">
     </div>
     <div class="py-1">
-        <textarea name="description" class="p-2 rounded border">
-            </textarea>
-    </div>
-    <div class="py-1">
-        <input type="submit" value="Create" class="p-2 border rounded">
+        <textarea name="description" class="p-2 rounded border"></textarea>
     </div>
 
+    <div class="py-2">
+        <div class="flex justify-center pb-4 px-4">
+            <h2 class="text-lg pb-4">Add steps if required</h2>
+            <span class="fas fa-plus px-2 py-2 cursor pointer">
+        </div>
+        <div class="py-1">
+            <input type="text" name="step" id="step" class="py-2 border rounded">
+        </div>
+    </div>
 </form>
 @endsection
--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,16 @@
     <title>Todos</title>
     <link rel="stylesheet" href="{{ asset('css/tailwind.min.css') }}">
     <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    @livewireStyles
 </head>
+
 <body>
     <div class="text-center pt-10 flex justify-center">
         <div class="w-1/3 border border-gray-400 rounded py-4">
             @yield('content')
         </div>
     </div>
+    @livewireScripts
 </body>
+
 </html>
